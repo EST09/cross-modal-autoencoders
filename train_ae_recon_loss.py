7e83b6b8ea8d0e9282923932074cb3ac4bbe4b22
Emily Thomas <thomas@maths.ox.ac.uk>
2022-02-01 14:06:20 +0000

    more plotting

--- train_ae_recon_loss.py
+++ train_ae_recon_loss.py
@@ -7,14 +7,21 @@ with open("/home/thomas/github/cross-modal-autoencoders/save_dir/train_ae_log.tx
 
 test_loss = []
 epoch = []
+train_loss = []
 
 for txt in data:
     if txt.split()[0] == "Test":
         test_loss.append(float(txt.split()[3]))
     elif txt.split()[0] == "Epoch:":
         epoch.append(float(txt.split()[1]))
+        train_loss.append(float(txt.split()[4]))
 
-epoch.append(epoch[-1]+1)
-plt.plot(epoch, test_loss)
+fig, axs = plt.subplots(2)
+axs[0].plot(epoch, test_loss[1:])
+axs[0].set_ylabel("test set loss")
+axs[1].plot(epoch, train_loss)
+axs[1].set_ylabel("train set loss")
+plt.xlabel("epoch")
 plt.show()
+
 
